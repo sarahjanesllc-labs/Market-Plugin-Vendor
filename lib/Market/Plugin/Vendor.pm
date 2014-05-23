31ffff56a344706725771a60ac3fe80f87caa265
Adam Stokes <adam.stokes@ubuntu.com>
2014-05-23 15:57:18 -0400

    work on routes
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- lib/Market/Plugin/Vendor.pm
+++ lib/Market/Plugin/Vendor.pm
@@ -1,7 +1,34 @@
 package Market::Plugin::Vendor;
 
-# ABSTRACT: Market::Plugin::Vendor is ...
+# ABSTRACT: vendor plugin for 2market
 
-use Mojo::Base -base;
+use Mojo::Base 'Mojolicious::Plugin';
+
+sub register {
+    my ($self, $app) = @_;
+
+    push @{$app->routes->namespaces}, 'Market::Plugin::Manager';
+    $app->helper(
+        get_vendor => sub {
+            my $self        = shift;
+            my $vendor_user = $self->db->namespace('vendors')
+              ->find_one({username => $self->session->{username}});
+        }
+    );
+    my $vendor = $app->routes->under(
+        sub {
+            my $self = shift;
+            return $self->auth_role_fail unless $self->session->{username};
+            return $self->auth_role_fail unless $self->get_vendor;
+        }
+    );
+
+    $app->routes->any('/explore')->to('explore#index')->name('explore_index');
+    $app->routes->any('/explore/:vendor')->to('explore#vendor')
+      ->name('explore_vendor');
+    $app->routes->any('/shop/:vendor')->to('shop#index')->name('shop_index');
+    $app->routes->any('/shop/:vendor/:product')->to('shop#detail')
+      ->name('shop_detail');
+}
 
 1;
